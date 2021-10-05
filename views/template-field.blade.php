ea5917688de5d61615b35f92ad67f038ad838867
Patrick Boivin <pboivin@gmail.com>
2021-10-05 16:35:48 -0400

    Add support for capsules

--- views/template-field.blade.php
+++ views/template-field.blade.php
@@ -1,4 +1,4 @@
-{{-- 
+{{--
     @param ?bool $templateFieldEditAfterCreate  Enable editing the field after the record is created.
     @param ?bool $templateFieldHideAfterCreate  Hide the field after the record is created.
     @param ?string $templateFieldModel  The fully qualified model class for this field.
@@ -9,8 +9,16 @@
     $templateFieldHideAfterCreate = $templateFieldHideAfterCreate ?? false;
 
     if (!isset($templateFieldModel)) {
+        $_tft_namespace = Config::get('twill.namespace', 'App');
+        $_tft_capsule_name = Str::studly($moduleName);
         $_tft_model_name = Str::studly(Str::singular($moduleName));
-        $templateFieldModel = Config::get('twill.namespace', 'App') . '\\Models\\' . $_tft_model_name;
+        $_tft_capsule_model_fqcn = "{$_tft_namespace}\\Twill\\Capsules\\{$_tft_capsule_name}\\Models\\{$_tft_model_name}";
+
+        $templateFieldModel = "{$_tft_namespace}\\Models\\{$_tft_model_name}";
+
+        if (class_exists($_tft_capsule_model_fqcn)) {
+            $templateFieldModel = $_tft_capsule_model_fqcn;
+        }
     }
 
     $_tft_instance = App::make($templateFieldModel);
--- views/template-form.blade.php
+++ views/template-form.blade.php
@@ -1,13 +1,17 @@
-{{-- 
+{{--
     @param ?string $templateFormViewPrefix  The full view prefix used to locate template partials.
 --}}
 
 @php
-    $templateFormViewPrefix = $templateFormViewPrefix ?? "admin";
+    $templateFormViewPrefix = $templateFormViewPrefix ?? "admin.{$moduleName}";
+
+    $_tft_capsule_name = Str::studly($moduleName);
 @endphp
 
 @includeFirst([
-    "{$templateFormViewPrefix}.{$moduleName}._{$item->current_template_value}",
-    "{$templateFormViewPrefix}.{$moduleName}._default",
+    "{$templateFormViewPrefix}._{$item->current_template_value}",
+    "{$templateFormViewPrefix}._default",
+    "{$_tft_capsule_name}.resources.views.admin._{$item->current_template_value}",
+    "{$_tft_capsule_name}.resources.views.admin._default",
     "twill-form-templates::_template-not-found",
 ])
