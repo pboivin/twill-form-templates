4463bf30327fa7ae2ef77694af53f75cc8fdb9e8
Patrick Boivin <pboivin@gmail.com>
2021-10-05 15:07:52 -0400

    Cleanup templates

--- views/template-field.blade.php
+++ views/template-field.blade.php
@@ -1,29 +1,35 @@
-@php
-    $tft_edit_after_create = $tft_edit_after_create ?? false;
-    $tft_hide_after_create = $tft_hide_after_create ?? false;
+{{-- 
+    @param ?bool $templateFieldEditAfterCreate  Enable editing the field after the record is created.
+    @param ?bool $templateFieldHideAfterCreate  Hide the field after the record is created.
+    @param ?string $templateFieldModel  The fully qualified model class for this field.
+--}}
 
-    if (!isset($tft_model)) {
-        $tft_model_name = Str::studly(Str::singular($moduleName));
-        $tft_model = Config::get('twill.namespace', 'App') . '\\Models\\' . $tft_model_name;
+@php
+    $templateFieldEditAfterCreate = $templateFieldEditAfterCreate ?? false;
+    $templateFieldHideAfterCreate = $templateFieldHideAfterCreate ?? false;
+
+    if (!isset($templateFieldModel)) {
+        $_tft_model_name = Str::studly(Str::singular($moduleName));
+        $templateFieldModel = Config::get('twill.namespace', 'App') . '\\Models\\' . $_tft_model_name;
     }
 
-    $tft_instance = App::make($tft_model);
-    $tft_should_edit = !isset($item) || $tft_edit_after_create;
+    $_tft_instance = App::make($templateFieldModel);
+    $_tft_should_edit = !isset($item) || $templateFieldEditAfterCreate;
 @endphp
 
-@if (!$tft_should_edit)
-    @if (!$tft_hide_after_create)
+@if (!$_tft_should_edit)
+    @if (!$templateFieldHideAfterCreate)
         @formField('input', [
             'name' => 'current_template_label',
-            'label' => $tft_instance->template_field_label,
+            'label' => $_tft_instance->template_field_label,
             'disabled' => true,
         ])
     @endif
 @else
     @formField('select', [
-        'name' => $tft_instance->template_field_name,
-        'label' => $tft_instance->template_field_label,
-        'default' => $tft_instance->default_form_template,
-        'options' => $tft_instance->available_form_templates,
+        'name' => $_tft_instance->template_field_name,
+        'label' => $_tft_instance->template_field_label,
+        'default' => $_tft_instance->default_form_template,
+        'options' => $_tft_instance->available_form_templates,
     ])
 @endif
--- views/template-form.blade.php
+++ views/template-form.blade.php
@@ -1,9 +1,13 @@
+{{-- 
+    @param ?string $templateFormViewPrefix  The full view prefix used to locate template partials.
+--}}
+
 @php
-    $tft_view_prefix = $tft_view_prefix ?? "admin";
+    $templateFormViewPrefix = $templateFormViewPrefix ?? "admin";
 @endphp
 
 @includeFirst([
-    "{$tft_view_prefix}.{$moduleName}._{$item->current_template_value}",
-    "{$tft_view_prefix}.{$moduleName}._default",
+    "{$templateFormViewPrefix}.{$moduleName}._{$item->current_template_value}",
+    "{$templateFormViewPrefix}.{$moduleName}._default",
     "twill-form-templates::_template-not-found",
 ])
