baa30716f72b9e04662711f406b450e78a34bd47
Patrick Boivin <pboivin@gmail.com>
2023-02-09 16:21:32 -0500

    fix: Add support for Twill 3 view prefix

--- views/template-form.blade.php
+++ views/template-form.blade.php
@@ -3,14 +3,17 @@
 --}}
 
 @php
-    $templateFormViewPrefix = $templateFormViewPrefix ?? "admin.{$moduleName}";
+    $templateFormViewPrefix = $templateFormViewPrefix ?? "twill.{$moduleName}";
 
+    $_tft_legacy_form_view_prefix = "admin.{$moduleName}";
     $_tft_capsule_name = Str::studly($moduleName);
 @endphp
 
 @includeFirst([
     "{$templateFormViewPrefix}._{$item->current_template_value}",
     "{$templateFormViewPrefix}._default",
+    "{$_tft_legacy_form_view_prefix}._{$item->current_template_value}",
+    "{$_tft_legacy_form_view_prefix}._default",
     "{$_tft_capsule_name}.resources.views.admin._{$item->current_template_value}",
     "{$_tft_capsule_name}.resources.views.admin._default",
     "twill-form-templates::_template-not-found",
